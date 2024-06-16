d4b42da2562d86f9da524c2d9cbe120f628b793b
belkhadir <belkhadir.anas@gmail.com>
2024-06-16 12:17:43 +0100

    it's causing bug not displaying more videos

--- Source/MovieDesign/Movies/ViewModel/MoviesViewModel.swift
+++ Source/MovieDesign/Movies/ViewModel/MoviesViewModel.swift
@@ -69,23 +69,11 @@ extension MoviesViewModel: MoviesDisplayable {
 private extension MoviesViewModel {
     @MainActor
     func appendMovies(_ movies: [MovieProviding]) {
-        let uniqueMovies = (moviesProvider + movies).unique()
-        moviesProvider = uniqueMovies
+        moviesProvider += movies
     }
 
     @MainActor
     func updateLoadingState(to state: LoadingState) {
         loadingState = state
-    }
-}
-
-// MARK: - Extension + [MovieProviding]
-private extension [MovieProviding] {
-    func unique() -> [Element] {
-        var seen = [Int: Element]()
-        for movie in self {
-            seen[movie.id] = movie
-        }
-        return Array(seen.values)
     }
 }
